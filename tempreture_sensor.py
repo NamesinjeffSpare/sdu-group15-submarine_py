8a9bf60fa39ec4e635db6f85fa97b48f106ebab1
NamesinjeffSpare <namesinjeff@gmail.com>
2025-12-14 16:31:14 +0100

    e
    e

--- tempreture_sensor.py
+++ tempreture_sensor.py
@@ -1,27 +1,9 @@
+from gpiozero import DHT11
+from time import sleep
 
-import time
-import board
-import adafruit_dht # to read from the DHT sensor
-
-# sensor data pin to gpio4
-
-sensor = adafruit_dht.DHT11(board.D4)
+sensor = DHT11(4)
 
 while True:
-    try:
-        # print the values to serial port
-        temperature_c = sensor.temperature
-        temperature_f = temperature_c * (9 / 5) + 32
-        humidity = sensor.humidity
-        print("Temp={0:0.1f}ºC, Temp={1:0.1f}ºF, Humidity={2:0.1f}%".format(temperature_c, temperature_f, humidity))
-
-    except RuntimeError as error:
-        # errors are often; DHTs are hard to read, just keep going
-        print(error.args[0])
-        time.sleep(2.0)
-        continue
-    except Exception as error:
-        sensor.exit()
-        raise error
-
-    time.sleep(3.0)
+    sensor.measure()
+    print(sensor.temperature, sensor.humidity)
+    sleep(3)
