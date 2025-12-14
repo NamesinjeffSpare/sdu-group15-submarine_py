66dd45658b334ce21ad0e2ecf757a117db4ec66e
NamesinjeffSpare <namesinjeff@gmail.com>
2025-12-14 20:51:42 +0100

    h
    hj

--- flash_test.py
+++ flash_test.py
@@ -17,6 +17,9 @@ try:
         print(f"Blink {i+1}: ON")
         time.sleep(ON_TIME)
 
+        GPIO.output(FLASHLIGHT_PIN, GPIO.LOW)   # flashlight OFF
+        print(f"Blink {i+1}: OFF")
+        time.sleep(OFF_TIME)
 
 finally:
     GPIO.output(FLASHLIGHT_PIN, GPIO.LOW)
